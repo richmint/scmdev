ad4720b925acf67591263f7f515da9845da8eec9
Babulal <rmwtoken5@richmint.com>
2022-07-05 12:52:57 +0530

    commit 2.1

---
+++ react-admin/src/Front/supplier/ApproveSupply.jsx
@@ -0,0 +1,88 @@
+import React,{useEffect, useState,useContext} from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import {useForm} from 'react-hook-form';
+import './AddMaterialForm.scss'
+//import { fetch, success } from './LoginSlice'
+//import "./login.scss"
+import { useNavigate } from "react-router-dom";
+
+import "../../pages/new/new.scss";
+import Navbar from "../../components/front_navbar/Navbar";
+import Sidebar from "../../components/front_sidebar/Sidebar";
+
+import { DarkModeContext } from "../../context/darkModeContext";
+
+const ApproceMaterialsupplier = ({ inputs, title, value }) => {
+  const { dispatch, metaMask, warehouseContract } = useContext(DarkModeContext);
+  const navigate = useNavigate();  
+  const {register, handleSubmit, formState: { errors }} = useForm({
+    defaultValues: {
+      pollyster: '',
+      cotton: '',
+      wool: ''
+    }
+  })
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [allWarehouse, setAllWarehouse] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [whContract, setContract] = useState(warehouseContract);
+  const [user,setUser] = useState(JSON.parse(sessionStorage.getItem('user'))); 
+  
+  
+
+  //console.log("whContract",whContract);
+  const addWarehouseHandler = (event) => {
+    console.log("The coming dats is ",event);
+    event.preventDefault();
+    
+    // // console.log(whContract);
+    //  console.log('sending ' + event.target.hashAddress.value + ' to the whContract');
+    // warehouseContract.addWarehouse(event.target.hashAddress.value);
+
+    //   const requestOptions = {
+    //       method: 'POST',
+    //       headers: { 'Content-Type': 'application/json' },
+    //       body: JSON.stringify({
+    //         "hashAddress":event.target.hashAddress.value,
+    //         "name":event.target.name.value,
+    //         "email":event.target.email.value,
+    //         "address":event.target.location.value,
+    //         "password":event.target.hashAddress.value,
+    //         "role":'Warehouse'
+    //         })
+    //   };
+    //   fetch('http://162.215.222.118:5150/register', requestOptions)
+    //       .then(response => response.json());
+     
+  }
+  
+
+ 
+  return (
+    <div className="new">
+       <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">          
+          <h1>Hi, {user && user.username}</h1>
+        </div>
+        <div className="bottom">
+          <div className="right">
+            <form onSubmit={handleSubmit(addWarehouseHandler)}>
+              <div className="formInput">
+                <label>You have to approve this plateform to manage your supplychain tokens for the factory to buy. This is to be done only once.</label>
+                
+              </div>
+              <div className='formInput'>
+              <button type={"submit"}> Approve </button>
+              {/* <span className='left'><button  type='reset' >Reset</button></span> */}
+              </div>          
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ApproceMaterialsupplier;
