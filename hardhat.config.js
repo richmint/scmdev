06bedd693ad4a18407cdd16429b197c491222596
Alankrit nokhwal <alankritnokhwal@gmail.com>
2022-07-08 21:06:43 +0530

    network rinkeby added

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,7 +25,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   rinkeby: {
     url: "https://speedy-nodes-nyc.moralis.io/3b50a8f528f7397fd9f310cf/eth/rinkeby", //Infura url with projectId
     accounts: ["28c80c76dc8dbfb442d93503d7583f645d96881346129be7ef74c01a8ad13378","dc32242523cf610bf7c16b778a5629337e4a213cec81b837e05a2a34bd73e5b9"] // add the account that will deploy the contract (private key)
-   }
+    // ["anon.test","alankrit.test"]
+  }
   },
   paths:{
     artifacts: './react-admin/src/artifacts',
