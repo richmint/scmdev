b587a83d31e3575adcd3198a699116f7a071b5fc
DESKTOP-H6R6B8M\Admin <rmwtoken5@richmint.com>
2022-07-13 17:47:15 +0530

    updated from

--- react-admin/src/components/front_navbar/Navbar.jsx
+++ react-admin/src/components/front_navbar/Navbar.jsx
@@ -16,9 +16,8 @@ const Navbar = (props) => {
 	// console.log("User name is ",userName)
 
 
-
-	let supplyChainTokenAddress = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
-	let supplyChainAddress = '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853';
+	let supplyChainTokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+	let supplyChainAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 
 
 	const { dispatch, metaMask, supplyChainContract, supplyChainTokenContract, ownerSupplyChainAddress } = useContext(DarkModeContext);
